69a669b7cd26f439825fafce4111611c9dd274ef
dannyap74 <danny.paleyev@gmail.com>
2019-07-02 17:15:59 +0300

    Initial Commit

---
+++ Win ICPC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Win ICPC
+//
+//  Created by Даниил Палеев on 02/07/2019.
+//  Copyright © 2019 Даниил Палеев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

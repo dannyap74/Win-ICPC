de32225f7eda71ad43112dc92bfabc944712caef
dannyap74 <danny.paleyev@gmail.com>
2019-07-02 21:27:00 +0300

    First commit

---
+++ Win ICPC/ThemeListViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ThemeListViewController.swift
+//  Win ICPC
+//
+//  Created by Даниил Палеев on 02/07/2019.
+//  Copyright © 2019 Даниил Палеев. All rights reserved.
+//
+
+import UIKit
--- Win ICPC/ViewController.swift
+++ Win ICPC/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.dataSource = self
     }
 
+}
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
 }
 
